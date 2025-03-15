e2febb01122f0c6d03d2187a5d8b6ed0226dbc63
Shon <onshad@gmail.com>
2025-03-15 16:05:03 +0200

    v2

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
-# Сутності:
+# Завдання 1
+# По перше додамо додатковий функціонал до класів з попередньої домашньої роботи:
+# Додайте поле birthday для дня народження в клас Record . Це поле має бути класу Birthday. Це поле не обов'язкове, але може бути тільки одне.
+# Додайте функціонал роботи з Birthday у клас Record, а саме функцію add_birthday, яка додає день народження до контакту.
+# Додайте функціонал перевірки на правильність наведених значень для полів Phone, Birthday.
+# Додайте та адаптуйте до класу AddressBook нашу функцію з четвертого домашнього завдання, тиждень 3, get_upcoming_birthdays, 
+# яка для контактів адресної книги повертає список користувачів, яких потрібно привітати по днях на наступному тижні.
 
-# Field: Базовий клас для полів запису.
-# Name: Клас для зберігання імені контакту. Обов'язкове поле. 
-# Phone: Клас для зберігання номера телефону. Має валідацію формату (10 цифр). (PhoneNumber)
-# Record: Клас для зберігання інформації про контакт, включаючи ім'я та список телефонів. (Contact)
-# AddressBook: Клас для зберігання та управління записами. (ContactList)
-
-# Функціональність:
-# AddressBook:Додавання записів.
-# Пошук записів за іменем.
-# Видалення записів за іменем.
-# Record:Додавання телефонів.
-# Видалення телефонів.
-# Редагування телефонів.
-# Пошук телефону.
+# Завдання 2
+# Для реалізації нового функціоналу також додайте функції обробники з наступними командами:
+# add-birthday - додаємо до контакту день народження в форматі DD.MM.YYYY                           +
+# show-birthday - показуємо день народження контакту
+# birthdays - повертає список користувачів, яких потрібно привітати по днях на наступному тижні
 
 import re
 from decorators import error_decorator
+import datetime as dt
+import pandas as pd
+
+def next_birthday(birthday, date):
+    if birthday.replace(year = date.year) >= date: 
+        return birthday.replace(year = date.year)
+    else:
+        return birthday.replace(year = date.year + 1)
+
+def get_upcoming_birthdays(users):
+    result = []
+    now_date = dt.datetime.now().date()
+    users_df = pd.DataFrame(users)
+    users_df['birthday'] =  users_df['birthday'].apply(lambda x: dt.datetime.strptime(x, '%d-%m-%Y').date())
+    users_df['next_birthday'] = users_df['birthday'].apply(lambda x: next_birthday(x, now_date))
+    users_df['delta'] = users_df['birthday'].apply(lambda x: (next_birthday(x, now_date) - now_date).days)
+    for i, r in users_df.iterrows():
+        if r['delta'] <= 7:
+            congrats_date = r['next_birthday']
+            if r['next_birthday'].weekday() in [5, 6]: 
+                congrats_date = r['next_birthday'] + dt.timedelta(days=(7 - r['next_birthday'].weekday()))
+            result.append({'name': f'{r['name']}', 'congratulation_date': f'{dt.datetime.strftime(congrats_date, '%d-%m-%Y')}'})
+
+    return result
+
+
+
 
 class Field:
     def __init__(self, value):
@@ -24,6 +48,12 @@ class Field:
 
     def __str__(self):
         return str(self.value)
+    
+@error_decorator(default_result=None) 
+class Birthday(Field):
+    def __init__(self, value):
+        tvalue = dt.datetime.strptime(value, '%d-%m-%Y').date()
+        super().__init__(tvalue)
 
 class PhoneNumber(Field):
     def __init__(self, phone_number):
@@ -44,6 +74,7 @@ class Contact():
     def __init__(self, args):
         self.name = ContactName(args[0])
         self.phones = []
+        self.birthday = None
 
     def __str__(self):        
         return f'Contact name: {self.name.value}, phones: {[p.value for p in self.phones]}'
@@ -90,6 +121,15 @@ class Contact():
             return f'{args[1]} found in contact: {self.name.value}'
         else: 
             return f'{args[1]} not found in contact: {self.name.value}'
+    
+    @error_decorator(default_result=None)  
+    def add_birthday(self, args): # Додаемо Birthday
+        self.birthday = Birthday(args[1])
+        return f'Birthday - {self.birthday.value.strftime('%d-%m-%Y')} added for contact {self.name}'
+    
+    @error_decorator(default_result=None)  
+    def show_birthday(self, args): # Додаемо Birthday        
+        return f'{self.birthday.value.strftime('%d-%m-%Y')}'
             
 
 class ContactList():
@@ -189,6 +229,14 @@ def main():
                 c = cl.get_contact(args)
                 if c != None:
                     print(c.find_phone(args))
+            case "add_birthday" | "ab":
+                c = cl.get_contact(args)
+                if c != None:
+                    print(c.add_birthday(args))
+            case "show_birthday" | "sb":
+                c = cl.get_contact(args)
+                if c != None:
+                    print(c.show_birthday(args))
 
 
 
